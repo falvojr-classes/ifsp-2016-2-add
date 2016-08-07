36ee1fd65364577cd0069501000df7bb25b120ae
Venilton FalvoJr <falvojr@gmail.com>
2016-08-07 11:52:05 -0300

    Add files via upload

---
+++ exercicio03.sql
@@ -0,0 +1,79 @@
+-- 2016-08-03
+
+USE cinema;
+
+-- Busque todos os filmes lançados até o ano 2000:
+
+SELECT * FROM filmes
+WHERE ano <= 2000;
+
+-- Busque todos os filmes que sejam da categoria “Terror”:
+
+UPDATE filmes
+SET categoria = 'Terror'
+WHERE id = 3;
+
+SELECT * FROM filmes;
+WHERE categoria = 'Terror';
+
+-- Busque todos os filmes que contenham “as” em seu título:
+
+INSERT INTO filmes
+(titulo, categoria, duracao, diretor, sinopse, ano)
+VALUES ('As branquelas', 'Comédia', 60, 'Fulano', 'Minha sinopse...', 2013);
+
+SELECT * FROM filmes
+WHERE titulo LIKE '%as%';
+
+-- Busque os 5 primeiros filmes considerando uma ordenação crescente em seu título:
+
+SELECT * FROM filmes
+ORDER BY titulo ASC
+LIMIT 5;
+
+-- Busque os 5 primeiros filmes considerando uma ordenação decrescente em sua duração:
+
+SELECT * FROM filmes
+ORDER BY duracao DESC
+LIMIT 5;
+
+-- Busque todos os filmes laçados em 2016 que tenham mais de 1 hora de duração:
+
+SELECT * FROM filmes
+WHERE ano = 2016 AND duracao > 60;
+
+-- Busque apenas os títulos e anos de lançamento dos filmes cujo diretor começe com a letra “a”:
+
+SELECT titulo, ano FROM filmes
+WHERE diretor LIKE 'a%';
+
+-- Exclua todos os filmes com categoria igual a “Romance”:
+
+DELETE FROM filmes
+WHERE categoria = 'Romance';
+
+-- Atualize todos os filmes com sinopse nula para que passem a ser vazias:
+
+UPDATE filmes
+SET sinopse = ''
+WHERE sinopse IS NULL;
+
+-- Busque todos os filmes lançados entre os anos 2000 e 2010:
+
+SELECT * FROM filmes
+WHERE ano >= 2000 AND ano <= 2010;
+
+SELECT * FROM filmes
+WHERE ano BETWEEN 2000 AND 2010;
+
+
+
+
+
+
+
+
+
+
+
+
